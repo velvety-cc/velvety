4752e0ec253077400504ba8a8f64519c3077ec4f
Liam Z <liam@txlabs.org>
2022-06-22 23:34:12 -0700

    test NEXT.JS Link

---
+++ pages/essays/first-essay.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FirstEssay() {
+    return <>
+        <h1>First Essay</h1>
+        <Link href="/">
+        <a>Go back to home page</a>
+        </Link>
+    </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,8 +10,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className='title'>
+          Read {" "}
+          <Link href="essays/first-essay">
+            <a>essays</a>
+          </Link>
         </h1>
 
         <p className="description">

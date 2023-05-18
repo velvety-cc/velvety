ecee09a427f543502db7be1dccce1b89d0cf2ebd
Liam Zhang <imliamzhang@gmail.com>
2023-05-18 00:44:46 -0700

    refactor resume

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
-import About from '../../components/pages/about/about';
+import Link from 'next/link';
+import Header from '../../components/shared/header/header';
+import ExternalLinks from '../../components/shared/external-links/external-links';
 import { Metadata } from 'next';
 
 // Metadata
@@ -7,5 +9,94 @@ export const metadata: Metadata = {
 };
 
 export default function AboutPage() {
-    return <About></About>;
+    return (
+        <div className='flex flex-col gap-y-16'>
+            {/* Header */}
+            <Header></Header>
+
+            {/* Content container */}
+            <div className='flex flex-col gap-y-16 pb-16 sm:grid sm:grid-cols-3 sm:gap-x-8 sm:pb-32 lg:gap-x-16'>
+                {/* Content - left */}
+                <div className='flex flex-col gap-y-16 sm:col-span-2 sm:col-start-1'>
+                    {/* About */}
+                    <div className='mt-1 flex flex-col gap-y-5'>
+                        <p className='font-serif text-lg font-medium italic text-slate-950'>
+                            Velvety is a personal archive, designed and built by
+                            Liam Zhang.
+                        </p>
+                        <p className='font-serif text-lg font-medium italic text-slate-950'>
+                            It’s an ongoing experiment of different ideas. A
+                            wardrobe of designs, writings, and projects. A
+                            living collection of memories, thoughts, and
+                            conscience.
+                        </p>
+                    </div>
+
+                    {/* About Liam */}
+                    <div className='flex flex-col gap-y-8 '>
+                        <div className='text-2xl font-medium text-slate-950'>
+                            About Liam
+                        </div>
+                        <div>
+                            <p className='mb-0 mt-0 text-base font-normal text-slate-950'>
+                                On a daily basis, I am the Co-Founder and Chief
+                                Product Officer at{' '}
+                                <Link
+                                    href='http://blockless.network'
+                                    target='_blank'
+                                    className='text-base font-normal text-slate-950 underline decoration-neutral-400 underline-offset-4 duration-200 hover:decoration-slate-950'
+                                >
+                                    Blockless
+                                </Link>
+                                , a decentralized compute platform helping users
+                                to build and ship the next generation of
+                                applications.
+                            </p>
+                            <p className='mb-0 mt-6 text-base font-normal text-slate-950'>
+                                Previously, my work spanned financial research,
+                                investment banking, and primary market
+                                investing. Take a look at my{' '}
+                                <Link
+                                    href='/about/resume'
+                                    className='text-base font-normal text-slate-950 underline decoration-neutral-400 underline-offset-4 duration-200 hover:decoration-slate-950'
+                                >
+                                    resume
+                                </Link>{' '}
+                                if you are interested.
+                            </p>
+                            <p className='mb-0 mt-6 text-base font-normal text-slate-950'>
+                                Heavily concentrated in finance early in my
+                                career, I like to consider myself now as someone
+                                who creates.
+                            </p>
+                            <p className='mb-0 mt-6 text-base font-normal text-slate-950'>
+                                In my spare time, I enjoy product design,
+                                software engineering, automobile engineering
+                                (slowly getting started), longevity
+                                (experimenting), interior/furniture design
+                                (Pierre Jeanneret in particular), and many more.
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* About this website */}
+                    <div className='flex flex-col gap-y-8 '>
+                        <div className='text-2xl font-medium text-slate-950'>
+                            About This Website
+                        </div>
+                        <p className='mb-0 mt-0 text-base font-normal text-slate-950'>
+                            This website is designed using Figma and Framer,
+                            built with Next.js and Tailwind CSS, and is hosted
+                            on Vercel.
+                        </p>
+                    </div>
+                </div>
+
+                {/* External links */}
+                <div className='row-start-1 sm:col-span-1 sm:col-start-3'>
+                    <ExternalLinks></ExternalLinks>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/app/about/resume/page.tsx
+++ src/app/about/resume/page.tsx
@@ -1,5 +1,8 @@
-import Resume from '../../../components/pages/resume/resume';
-
+import Link from 'next/link';
+import Image from 'next/image';
+import BackArrow from '../../../../public/back-arrow.svg';
+import Header from '../../../components/shared/header/header';
+import ExternalLinks from '../../../components/shared/external-links/external-links';
 import { Metadata } from 'next';
 
 // Metadata
@@ -8,5 +11,229 @@ export const metadata: Metadata = {
 };
 
 export default function ResumePage() {
-    return <Resume></Resume>;
+    function renderEducationItem(school: String, year: String, degree: String) {
+        return (
+            <div>
+                <div className='mb-2 flex flex-col gap-y-1  sm:flex-row sm:items-start sm:justify-between'>
+                    <span className='inline-block text-base font-semibold text-slate-950'>
+                        {school}
+                    </span>
+                    <span className='inline-block shrink-0 text-base font-normal text-slate-950 sm:ml-8 md:ml-16'>
+                        {year}
+                    </span>
+                </div>
+                <p className='font-serif text-base font-normal italic text-slate-950'>
+                    {degree}
+                </p>
+            </div>
+        );
+    }
+
+    function renderExperienceItem(
+        role: String,
+        firm: String,
+        link: boolean,
+        url: Object,
+        year: String,
+        description: String
+    ) {
+        return (
+            <div>
+                <div className='mb-2 flex flex-col gap-y-1 sm:flex-row sm:items-start sm:justify-between'>
+                    <span className='inline-block text-base font-semibold text-slate-950'>
+                        {role} /{' '}
+                        {link ? (
+                            <Link
+                                href={url}
+                                target='_blank'
+                                className='text-base font-semibold text-slate-950 underline decoration-neutral-400 underline-offset-4 duration-200 hover:decoration-slate-950'
+                            >
+                                {firm}
+                            </Link>
+                        ) : (
+                            firm
+                        )}
+                    </span>
+                    <span className='inline-block shrink-0 text-base font-normal text-slate-950 sm:ml-8 md:ml-16'>
+                        {year}
+                    </span>
+                </div>
+
+                <div className='text-base font-normal text-slate-950'>
+                    {description}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className='flex flex-col gap-y-16'>
+            {/* Header */}
+            <Header></Header>
+
+            {/* Content container */}
+            <div className='flex flex-col gap-y-16 pb-16 sm:grid sm:grid-cols-3 sm:gap-x-8 sm:pb-32 lg:gap-x-16'>
+                {/* Content - left */}
+                <div className='flex flex-col gap-y-8 sm:col-span-2 sm:col-start-1'>
+                    {/* Resume title */}
+                    <div className='flex flex-row items-end justify-between'>
+                        <div className='text-2xl font-medium text-slate-950'>
+                            Resume
+                        </div>
+                        <Link
+                            href='/about'
+                            className='text-base font-normal text-slate-950'
+                        >
+                            <span className='inline-block'>
+                                <Image src={BackArrow} alt='Back arrow'></Image>
+                            </span>{' '}
+                            Back to about
+                        </Link>
+                    </div>
+
+                    {/* Divider */}
+                    <div className='h-[2px] bg-slate-950'></div>
+
+                    {/* Education */}
+                    <div className='flex flex-col gap-y-6'>
+                        {/* Title */}
+                        <div className='text-2xl font-medium text-slate-950'>
+                            Education
+                        </div>
+
+                        {renderEducationItem(
+                            'Carnegie Mellon University, College of Engineering',
+                            '2023 - 2025',
+                            'M.Eng. in Electricial and Computer Engineering'
+                        )}
+
+                        {renderEducationItem(
+                            'New York University, Gallatin School of Individualized Study',
+                            '2018 - 2022',
+                            'B.A. in Finance, Computer Science, and Mathematics'
+                        )}
+                    </div>
+
+                    {/* Divider */}
+                    <div className='h-[2px] bg-slate-950'></div>
+
+                    {/* Professional experience */}
+                    <div className='flex flex-col gap-y-6'>
+                        <div className='text-2xl font-medium text-slate-950'>
+                            Professional Experience
+                        </div>
+
+                        {renderExperienceItem(
+                            'Co-Founder and Chief Product Officer',
+                            'Blockless',
+                            true,
+                            'https://blockless.network',
+                            '2021 - Present',
+                            'Directed the ideation, design, and implementation of Blockless, a decentralized compute platform.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Investor and Researcher',
+                            'NGC Ventures',
+                            true,
+                            'https://ngc.fund',
+                            '2020 - 2021',
+                            'Conducted research and investment on decentralized finance and infrastructure projects.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Researcher',
+                            'Guosheng Group Blockchain Institute',
+                            false,
+                            '',
+                            '2020 - 2021',
+                            'Conducted research on various Web3 protocols.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Researcher',
+                            'New York University',
+                            true,
+                            'https://nyu.edu',
+                            '2020 - 2021',
+                            'Conducted research on topics including the dynamics of the DeFi protocols and the efficiency of the Chinese options market.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Investment Analyst',
+                            'Befor Capital',
+                            true,
+                            'http://www.beforcapital.com/',
+                            '2019',
+                            'Researched target companies and managed the venture capital investment process.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Investment Analyst',
+                            'Gaoyun Ventures',
+                            false,
+                            '',
+                            '2018',
+                            'Private equity investment.'
+                        )}
+
+                        {renderExperienceItem(
+                            'Investment Banking Analyst',
+                            'GF Securities',
+                            true,
+                            'https://en.gf.com.cn/',
+                            '2017',
+                            'Performed financial analysis and modeling for pre-IPO clients.'
+                        )}
+                    </div>
+
+                    {/* Divider */}
+                    <div className='h-[2px] bg-slate-950'></div>
+
+                    {/* Publications */}
+                    <div className='flex flex-col gap-y-6'>
+                        <div className='text-2xl font-medium text-slate-950'>
+                            Publications
+                        </div>
+
+                        <div className='flex flex-row gap-x-2 font-serif'>
+                            <div className='inline-block items-start text-xs font-light text-slate-950'>
+                                1
+                            </div>
+                            <div className='text-base font-normal text-slate-950'>
+                                Chi, Y., Hao, W., & Zhang, Y. (2022). Volatility
+                                model applications in China’s SSE50 options
+                                market. <em>Journal of Futures Markets</em>, 42,
+                                1704– 1720.{' '}
+                                <Link
+                                    href='https://doi.org/10.1002/fut.22294'
+                                    target='_blank'
+                                    className='italic'
+                                >
+                                    https://doi.org/10.1002/fut.22294
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-row gap-x-2 font-serif'>
+                            <div className='inline-block items-start text-xs font-light text-slate-950'>
+                                2
+                            </div>
+                            <div className='text-base font-normal text-slate-950'>
+                                Zhang, Y., Junque de Fortuny, E. (2022). State
+                                of the DeFi Landscape: Charting the Global DeFi
+                                Ecosystem. <em>Service Science</em>, Under
+                                Review
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* External links */}
+                <div className='hidden sm:col-span-1 sm:col-start-3 sm:row-start-1 sm:block'>
+                    <ExternalLinks></ExternalLinks>
+                </div>
+            </div>
+        </div>
+    );
 }
